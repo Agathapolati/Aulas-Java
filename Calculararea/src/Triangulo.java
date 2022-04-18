7cfc66f1ec4224ba79212f60d8a42029d5cc5618
Agatha Polati <102962770+Agathapolati@users.noreply.github.com>
2022-04-18 11:03:29 -0300

    Add files via upload

---
+++ Calculararea/src/Triangulo.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Triangulo {
+	
+}
+
+Scanner scanner = new Scanner(System.in);
+
+
+Triangulo triangulo = new Triangulo();
+
+System.out.println("Multiplicar dois Números");
+System.out.println("Digite a base do Triangulo");
+int x = scanner.nextInt();
+
+System.out.println("Digite a altura do Triangulo");
+int y = scanner.nextInt();
+
+int  = matematica.somar(x, y);
+System.out.println("O resultado é " + soma + "!!!");

9a765daaf802a59f38d14a299f1450fd3b0e16ff
aluno <aluno@DSN1251230711>
2022-04-27 11:16:34 -0300

    Primeiro commit

---
+++ ExercicioLampada/src/Lampada.java
@@ -0,0 +1,17 @@
+
+public class Lampada {
+private boolean estado; // ligado ou não
+		
+public Lampada() {
+this.estado = true;
+}
+public void estado (boolean estado) {
+	this.estado = false;
+}
+public boolean estado() {
+	return this.estado;
+}
+
+}
+
+	

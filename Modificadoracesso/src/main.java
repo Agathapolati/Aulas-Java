cab3c46857dec03f0890586a17c0820f55da3b97
Agatha Polati <102962770+Agathapolati@users.noreply.github.com>
2022-05-02 11:38:32 -0300

    Add files via upload

--- Modificadoracesso/src/main.java
+++ Modificadoracesso/src/main.java
@@ -19,7 +19,47 @@ public class main {
 	    ContaBancaria conta2 = new ContaBancaria();
 	    System.out.println(conta2.saldo());
 		
-
+        Cliente cliente1 = new Cliente();
+        
+        cliente1.setNome("Agatha");
+        cliente1.setCpf(123456);
+        cliente1.setNumConta(33333);
+        
+        System.out.println(cliente1.getNome());
+        System.out.println(cliente1.getCpf());
+        System.out.println(cliente1.getNumConta());
+        
+        System.out.println(cliente1);
+        
+        Cliente cliente2 = new Cliente();
+        
+        cliente2.setNome("Agatha");
+        cliente2.setCpf(123456);
+        cliente2.setNumConta(33333);
+        
+       // cliente2 = cliente1;
+        
+        System.out.println(cliente2);
+        
+        
+        if(cliente1.getNome() == cliente2.getNome()) {
+        	System.out.println("Os Nomes são iguais");
+        }else {
+        	System.out.println("Os Nomes são diferentes");
+        }
+        
+        if(cliente1 == cliente2) { 
+        	System.out.println("Os objetos são iguais");
+        }else {
+        	System.out.println("Os objetos são diferentes");
+        }
+        
+        if(cliente1.equals(cliente2)) {
+        System.out.println("Os objetos são iguais");
+        }else {
+        System.out.println("Os objetos são diferentes");
+        	
+        }
 	}
 
 }

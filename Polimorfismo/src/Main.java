61f765cb982a0dcb81cf7d7154b22da727f5e97c
aluno <aluno@DSN1251017046>
2022-05-05 11:32:43 -0300

    Polimorfismo

---
+++ Polimorfismo/src/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Animal animal = new Cachorro();
+		animal.Falar();
+		
+		animal = new Gato();
+		animal.Falar();
+	}
+	
+	public void falar() {
+		 
+		
+	}
+
+}

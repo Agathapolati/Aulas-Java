bb41ea0e1dbb7381541b782af27419bd57ebea61
aluno <aluno@DSN1251017046>
2022-05-12 10:55:18 -0300

    Classe Abstrata

---
+++ ClassesAbstratas/src/Cogumelo.java
@@ -0,0 +1,11 @@
+
+public class Cogumelo extends Item{
+	
+
+	public void pegar() {
+		System.out.println("Pegou o Cogumelo +100 pontos");
+	}
+	
+
+
+}
---
+++ ClassesAbstratas/src/Item.java
@@ -0,0 +1,30 @@
+
+public abstract class Item {
+	
+	private int posicaoX = 1;
+	private int posicaoY = 1;
+	
+	public abstract void pegar();  //todos que herdarem a classe ITEM, devem ter o método pegar
+	//classe abstrata só tem a assinatura (;)
+	//se fosse implementar, teriam as chaves ({})
+	
+	
+	//Getter and Setter//
+
+	public int getPosicaoX() {
+		return posicaoX;
+	}
+
+	public void setPosicaoX(int posicaoX) {
+		this.posicaoX = posicaoX;
+	}
+
+	public int getPosicaoY() {
+		return posicaoY;
+	}
+
+	public void setPosicaoY(int posicaoY) {
+		this.posicaoY = posicaoY;
+	}
+
+}

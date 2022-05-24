08a92973f4d5d8b2d358afd3770ec2ebe1d2385a
Agatha Polati <102962770+Agathapolati@users.noreply.github.com>
2022-05-24 09:08:22 -0300

    Update Endereco.java

--- Cadastro/src/Endereco.java
+++ Cadastro/src/Endereco.java
@@ -1,4 +1,39 @@
 
-public class Endereco {
-
+class  public Endereco {
+	
+	privado  String  rua ;
+	 número int  privado ;
+	 bairro de String  privado ;
+	
+	
+	public  String  getRua () {
+		voltar  rua ;
+	}
+	
+	
+	public  void  setRua ( String  rua ) {
+		isso . rua = rua ;
+	}
+	
+	
+	public  int  getNumero () {
+		retornar  numero ;
+	}
+	
+	
+	public  void  setNumero ( int  numero ) {
+		isso . numero = numero ;
+	}
+	
+	
+	public  String  getBairro () {
+		 bairro de retorno ;
+	}
+	
+	
+	public  void  setBairro ( String  bairro ) {
+		isso . bairro = bairro ;
+	}
+	
+	
 }

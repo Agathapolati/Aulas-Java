d43d70a935e5b234eea4cf8afc6caa7763b53a33
Agatha Polati <102962770+Agathapolati@users.noreply.github.com>
2022-05-30 11:39:36 -0300

    Add files via upload
    Cadastro ler e escrever

---
+++ CadastroLereEscrever/src/Mouse.java
@@ -0,0 +1,50 @@
+
+public class Mouse {
+	private String funciona;
+	private String qtdBotoes;
+	private String tamanho;// Para Conseguir Ler Precisamos do Getters and Setters pois �  uma Classe Privada.
+	public Mouse(String funciona, String qtdBotoes, String tamanho) {
+		super();
+		this.funciona = funciona;
+		this.qtdBotoes = qtdBotoes;
+		this.tamanho = tamanho;
+	}
+	public String isFunciona() {
+		return funciona;
+	}
+	public void setFunciona(String funciona) {
+		this.funciona = funciona;
+	}
+	public String getQtdBotoes() {
+		return qtdBotoes;
+	}
+	public void setQtdBotoes(String qtdBotoes) {
+		this.qtdBotoes = qtdBotoes;
+	}
+	public String getTamanho() {
+		return tamanho;
+	}
+	public void setTamanho(String tamanho) {
+		this.tamanho = tamanho;
+	}
+	@Override
+	public String toString() {
+		return "Mouse: funciona=" + funciona + ", qtdBotoes=" + qtdBotoes + ", tamanho=" + tamanho + "\n"; // "\n " serve para pular linha.
+	} 
+	   public Mouse(String texto) {
+		   
+		   String[] arrayAuxiliar1 = texto.split(",");
+		   String[] arrayAuxiliar2 =  arrayAuxiliar1[0].split("="); //Split:Quando ele encontra a "," ele quebra em duas Strings.
+		   this.funciona = arrayAuxiliar2[1].trim(); // Trim:Espa�o em branco ele joga fora.
+		   
+		   String[] arrayAuxiliar3 = arrayAuxiliar1[1].split("="); //Quebra o =.
+		   this.qtdBotoes = arrayAuxiliar3[1].trim();
+		   
+		   String[] arrayAuxiliar4 = arrayAuxiliar1[2].split("=");
+		   this.tamanho = arrayAuxiliar4[1].trim(); // O "Trim" vai tirar o \n.
+		     
+				   
+	   }
+	
+
+}
---
+++ CadastroLereEscrever/src/main.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class main {
+
+	public static void main(String[] args) throws IOException { // IOEException serve para que se der algum erro ele "mostra" na tela.(se tiver algum erro na leitura ou na escrita ele avisa)
+		
+		List <Mouse> listaMouse = new ArrayList<Mouse>();
+		
+		Mouse mouse1 = new Mouse("sim", "5", "Pequeno"); //Criando os 3 Mouses
+		listaMouse.add(mouse1);
+		
+		Mouse mouse2 = new Mouse("sim", "5", "Medio");
+		listaMouse.add(mouse2);
+		
+		Mouse mouse3 = new Mouse("sim", "5", "Grande");
+		listaMouse.add(mouse3);
+		
+		try(BufferedWriter escrever = new BufferedWriter (new FileWriter("Mouses.txt"))){
+			for (Mouse mouse : listaMouse)
+				escrever.write(mouse.toString()); //Escrevendo
+		}
+		
+		List <Mouse> listaMouse2 = new ArrayList<Mouse>();
+		
+		try(BufferedReader reader = new BufferedReader (new FileReader("Mouses.txt"))){ //Lendo
+			String line;
+			String string = "";
+			
+			while ((line = reader.readLine()) != null) { //Retorna nulo. While:"Enquanto" então enquanto a condição for verdadeira vai executar.
+				string+= line + "\n"; //"\n" pula linha
+				Mouse mouse = new Mouse(line);
+				listaMouse2.add(mouse);
+			}
+			
+			for (Mouse mouse5 : listaMouse2) { //Vai executar até achar a condição falsa
+				System.out.println(mouse5);
+			}
+			
+			
+		}
+		
+		
+		
+		
+		
+		
+	}
+
+}

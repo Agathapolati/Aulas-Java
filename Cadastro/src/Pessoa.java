fd96e81dcc1449c5bcffc09f51ac939a01b933b4
Agatha Polati <102962770+Agathapolati@users.noreply.github.com>
2022-06-06 09:56:16 -0300

    Update Pessoa.java

--- Cadastro/src/Pessoa.java
+++ Cadastro/src/Pessoa.java
@@ -6,7 +6,7 @@ public class Pessoa {
 	private Sexo sexo;
 	private	Endereco endereco;
 
-	
+	//o método split quando divide, a primeira parte será 0 e outra será 1.
 
 	// <------------------------------Getter e Setter--------------------------------------->
 	public String getNome() { //Usando o Getter e Setter
@@ -47,18 +47,18 @@ public class Pessoa {
 	}
 	public Pessoa(String linha) {
 		
-		String[] arrayLinha = linha.split(",");
-		String[] arrayNome = arrayLinha[0].split("=");
+		String[] arrayLinha = linha.split(",");// faz split(repartição de variavel) da variavel line a partir da (","),(quebra).
+		String[] arrayNome = arrayLinha[0].split("=");//// faz split(repartição de variavel em duas partes) da variavel line a partir do ("=") e guarda na variavel ArrayNome
 		this.nome = arrayNome[1].trim();
 		
 		String[] arrayIdade = arrayLinha[1].split("=");
-		this.idade = Integer.parseInt(arrayIdade[1].trim());
+		this.idade = Integer.parseInt(arrayIdade[1].trim());// Retira os espaços da variavel ArrayIdade e guarda em idade.
 		
 		String[] arraySexo = arrayLinha[2].split("=");
-		this.sexo = Sexo.valueOf(arraySexo[1].trim());
+		this.sexo = Sexo.valueOf(arraySexo[1].trim()); // Retira os espaços da variável ArraySexo e guarda no atributo sexo.
 		
-		Endereco endereco = new Endereco(arrayLinha);
-		this.endereco = endereco;
+		Endereco endereco = new Endereco(arrayLinha);// Instância um ArrayLinha
+		this.endereco = endereco;//Um endereço variável é igual ao endereço de atributo
 		
 	}
 	

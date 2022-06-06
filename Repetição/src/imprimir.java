94a9e9c79511d8d30ee1ee5f373b306fe2d2cff7
Agatha Polati <102962770+Agathapolati@users.noreply.github.com>
2022-06-06 10:35:27 -0300

    Add files via upload

---
+++ Repetição/src/imprimir.java
@@ -0,0 +1,10 @@
+
+public class imprimir {
+
+	public static void main(String[] args) {
+		for ( int  i = 10 ; i < 26 ; i ++) { //for: que faz com que toda vez que a variavel "i"  que começa em 10, for menor que 26 adiciona mais 1 a "i" e mostra no console
+			  Sistema . fora . println ( i );
+		}
+	}
+
+}

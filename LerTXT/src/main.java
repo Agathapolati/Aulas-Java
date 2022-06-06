d7fff29a497d259e899635778f84e7551558d8fb
Agatha Polati <102962770+Agathapolati@users.noreply.github.com>
2022-06-06 09:15:05 -0300

    Add files via upload

--- LerTXT/src/main.java
+++ LerTXT/src/main.java
@@ -6,19 +6,19 @@ import java.io.IOException;
 public class main {
 
 	public static void main(String[] args) throws FileNotFoundException, IOException {
-		try(BufferedReader reader = new BufferedReader(new FileReader("entrada.txt"))){ // BufferedReader:Chama os metodos de outra aplicaÁ„o como o Main
+		try(BufferedReader reader = new BufferedReader(new FileReader("entrada.txt"))){ // BufferedReader:Chama os metodos de outra aplica√ß√£o como o Main
 			String line;
 			String string = "";
 			
-			// While:usasse pois n„o sabemos a quantidade de linhas que vamos utilizar.
-			while((line = reader.readLine())!= null) { //read line : leitura. Null: se a linha tiver vazia ele sai fora, n„o retorna nada.
+			// While:usasse pois n√£o sabemos a quantidade de linhas que vamos utilizar.
+			while((line = reader.readLine())!= null) { //read line : leitura. Null: se a linha tiver vazia ele sai fora, n√£o retorna nada.
 				string += line + "\n"; //+= peguei o valor e somei com o valor que ja estava dentro, vai colocar embaixo e pular a linha com o "\n".
 			}
 			System.out.println(string); //syso para mostar o falor da string 
 	}
 		String teste = "Mouse: funciona = sim, qtdBotoes= 2, tamanho = pequeno\n";
-		Mouse mouse = new Mouse(teste);
-		System.out.println(mouse);
+		Mouse mouse = new Mouse(teste); //Criando o objeto
+		System.out.println(mouse);//Mostrando na tela
 		
 		
 		

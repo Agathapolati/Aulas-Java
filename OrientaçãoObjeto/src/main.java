7ae1375b8a735e996182358fe010b90cdc1362ba
Agatha Polati <102962770+Agathapolati@users.noreply.github.com>
2022-06-06 09:35:30 -0300

    Add files via upload

--- OrientaçãoObjeto/src/main.java
+++ OrientaçãoObjeto/src/main.java
@@ -8,9 +8,10 @@ public class main {
 	   pessoa1.nome = "Agatha";
 	   pessoa1.numFigurinhas = 1;
 	   
-	   pessoa1.receber(3);
+	   pessoa1.receber(3); //recebe o 3
 	   
-	   System.out.println(pessoa1.nome);
+	   
+	   System.out.println(pessoa1.nome); //Mostrando as informções na tela
 	   System.out.println(pessoa1.numFigurinhas);
    }
 }

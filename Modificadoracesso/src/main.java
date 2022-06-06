664ec82a747b4c0f0b260d1291eef69aedbfa669
Agatha Polati <102962770+Agathapolati@users.noreply.github.com>
2022-06-06 10:30:15 -0300

    Update main.java

--- Modificadoracesso/src/main.java
+++ Modificadoracesso/src/main.java
@@ -19,19 +19,20 @@ public class main {
 	    ContaBancaria conta2 = new ContaBancaria();
 	    System.out.println(conta2.saldo());
 		
-        Cliente cliente1 = new Cliente();
+        Cliente cliente1 = new Cliente();//Cria o primeiro cliente
         
         cliente1.setNome("Agatha");
         cliente1.setCpf(123456);
         cliente1.setNumConta(33333);
-        
+		
+        //Mostra todos os dados do Cliente
         System.out.println(cliente1.getNome());
         System.out.println(cliente1.getCpf());
         System.out.println(cliente1.getNumConta());
         
         System.out.println(cliente1);
         
-        Cliente cliente2 = new Cliente();
+        Cliente cliente2 = new Cliente();//Cria o Segundo cliente
         
         cliente2.setNome("Agatha");
         cliente2.setCpf(123456);
@@ -43,21 +44,21 @@ public class main {
         
         
         if(cliente1.getNome() == cliente2.getNome()) {
-        	System.out.println("Os Nomes s„o iguais");
+        	System.out.println("Os Nomes s√£o iguais");
         }else {
-        	System.out.println("Os Nomes s„o diferentes");
+        	System.out.println("Os Nomes s√£o diferentes");
         }
         
         if(cliente1 == cliente2) { 
-        	System.out.println("Os objetos s„o iguais");
+        	System.out.println("Os objetos s√£o iguais");
         }else {
-        	System.out.println("Os objetos s„o diferentes");
+        	System.out.println("Os objetos s√£o diferentes");
         }
         
-        if(cliente1.equals(cliente2)) {
-        System.out.println("Os objetos s„o iguais");
-        }else {
-        System.out.println("Os objetos s„o diferentes");
+        if(cliente1.equals(cliente2)) {//Se os dados do cliente 1 for igual ao dos cliente 2 mostra no console que os dados s√£o iguais.
+        System.out.println("Os objetos s√£o iguais");
+        }else { // else:Se n√£o ele mostra na tela que os Objetos s√£o diferentes
+        System.out.println("Os objetos s√£o diferentes");
         	
         }
 	}

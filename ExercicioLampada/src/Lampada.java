0a5c9f030864fa26b9fb1bf26df67082ee70506f
Agatha Polati <102962770+Agathapolati@users.noreply.github.com>
2022-06-06 09:11:35 -0300

    Add files via upload

--- ExercicioLampada/src/Lampada.java
+++ ExercicioLampada/src/Lampada.java
@@ -1,15 +1,15 @@
 
 public class Lampada {
-private boolean estado; // ligado ou não
+private boolean estado; // ligado ou nÃ£o
 		
 public Lampada() {
-this.estado = true;
+this.estado = true; // se for verdadeiro
 }
 public void estado (boolean estado) {
-	this.estado = false;
+	this.estado = false;  // se for falso
 }
 public boolean estado() {
-	return this.estado;
+	return this.estado; //Retornando o estado da lampada se ela vai estar ligada ou desligada
 }
 
 }
--- ExercicioLampada/src/main.java
+++ ExercicioLampada/src/main.java
@@ -3,7 +3,7 @@ public class main {
 
 	public static void main(String[] args) {
 		
-		Lampada Lampada = new Lampada();
+		Lampada Lampada = new Lampada(); //Criando o Objeto lampada
 		
 
 	}

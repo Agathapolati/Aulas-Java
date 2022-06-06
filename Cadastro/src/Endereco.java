d84d8f19d8389c02d1f883493d9dd9ef205addbc
Agatha Polati <102962770+Agathapolati@users.noreply.github.com>
2022-06-06 10:10:07 -0300

    Update Endereco.java

--- Cadastro/src/Endereco.java
+++ Cadastro/src/Endereco.java
@@ -7,10 +7,10 @@ public class Endereco {
 	
 	
 	public Endereco(String[] arrayLinha) { //Criando o Construtor
-		String[] arrayRua = arrayLinha[3].split("=");
-		this.rua = arrayRua[1].trim();
+		String[] arrayRua = arrayLinha[3].split("="); //Quebra a variavel Line já partida no pessoa, ea partir da terceira parte de um "=" a divide em dois
+		this.rua = arrayRua[1].trim();//remove espaços do string e guarda no atributo rua.
 		
-		String[] arrayNumero = arrayLinha[4].split("=");
+		String[] arrayNumero = arrayLinha[4].split("=");//Quebra a variavel line e coloca a string em arrayNumero.
 		this.rua = arrayRua[1].trim();
 		
 		String[] arrayBairro = arrayLinha[5].split("=");
@@ -24,7 +24,7 @@ public class Endereco {
 	}
 		
 		
-		
+	//Override	
 	@Override
 	public String toString() {
 		return ", Endereco: rua=" + rua + ", numero=" + numero + ", bairro=" + bairro ;
